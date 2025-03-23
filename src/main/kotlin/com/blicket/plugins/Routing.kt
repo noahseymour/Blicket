c1229f4cf9d731f7d436670c6057688d36349ac4
Noah Seymour <noahseymour2006@gmail.com>
2025-03-23 01:57:49 +0100

    Re-built interface

--- src/main/kotlin/com/blicket/plugins/Routing.kt
+++ src/main/kotlin/com/blicket/plugins/Routing.kt
@@ -1,7 +1,6 @@
 package com.blicket.plugins
 
 import io.ktor.server.application.*
-import io.ktor.server.http.content.static
 import io.ktor.server.http.content.staticResources
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -9,7 +8,7 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     routing {
         staticResources("/templates", "templates")
-        staticResources("/styles", "styles")
+        staticResources("/css", "css")
         staticResources("/scripts", "scripts")
 
         route("/") {
